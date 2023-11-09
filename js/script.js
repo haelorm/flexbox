836b2f4882e192f8c7b339bf3269e157d35debbd
hanslaptop <hramoscampos@gmail.com>
2023-11-09 15:47:58 -0500

    commit 21

--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
 ((d) => {
-    const $botonesRadio = d.querySelectorAll("input[name='rowColumn']");
-    const $cRowColumn = d.getElementById('rowColumn');
+    const $botonesRadio = d.querySelectorAll("input[name='flex-direction']");
+    const $cflexDirection = d.getElementById('flex-direction');
     const seleccion = () => {
-        let botonSeleccionado = d.querySelector('input[name="rowColumn"]:checked');
-        if (botonSeleccionado.value === "c-row") {
-            $cRowColumn.classList.add("c-row");
-            $cRowColumn.classList.remove("c-row-reverse", "c-column", "c-column-reverse");
-        } else if (botonSeleccionado.value === "c-row-reverse") {
-            $cRowColumn.classList.add("c-row-reverse");
-            $cRowColumn.classList.remove("c-row", "c-column", "c-column-reverse");
-        } else if (botonSeleccionado.value === "c-column") {
-            $cRowColumn.classList.add("c-column");
-            $cRowColumn.classList.remove("c-row", "c-row-reverse", "c-column-reverse");
-        } else if (botonSeleccionado.value === "c-column-reverse") {
-            $cRowColumn.classList.add("c-column-reverse");
-            $cRowColumn.classList.remove("c-row", "c-row-reverse", "c-column");
+        let botonSeleccionado = d.querySelector('input[name="flex-direction"]:checked');
+        if (botonSeleccionado.value === "row") {
+            $cflexDirection.classList.add("row");
+            $cflexDirection.classList.remove("row-reverse", "column", "column-reverse");
+        } else if (botonSeleccionado.value === "row-reverse") {
+            $cflexDirection.classList.add("row-reverse");
+            $cflexDirection.classList.remove("row", "column", "c-column-reverse");
+        } else if (botonSeleccionado.value === "column") {
+            $cflexDirection.classList.add("column");
+            $cflexDirection.classList.remove("row", "row-reverse", "c-column-reverse");
+        } else if (botonSeleccionado.value === "column-reverse") {
+            $cflexDirection.classList.add("column-reverse");
+            $cflexDirection.classList.remove("row", "row-reverse", "column");
+        }
+    }
+    $botonesRadio.forEach(boton => {
+        boton.addEventListener("change", seleccion)
+    })
+    seleccion();
+})(document);
+
+((d) => {
+    const $botonesRadio = d.querySelectorAll("input[name='flex-wrap']");
+    const $cflexWrap = d.getElementById('flex-wrap');
+    const seleccion = () => {
+        let botonSeleccionado = d.querySelector('input[name="flex-wrap"]:checked');
+        if (botonSeleccionado.value === "nowrap") {
+            $cflexWrap.classList.add("nowrap");
+            $cflexWrap.classList.remove("wrap", "wrap-reverse");
+        } else if (botonSeleccionado.value === "wrap") {
+            $cflexWrap.classList.add("wrap");
+            $cflexWrap.classList.remove("nowrap", "wrap-reverse");
+        } else if (botonSeleccionado.value === "wrap-reverse") {
+            $cflexWrap.classList.add("wrap-reverse");
+            $cflexWrap.classList.remove("nowrap", "wrap");
         }
     }
     $botonesRadio.forEach(boton => {
