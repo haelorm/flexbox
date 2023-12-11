9a179dc4ce8c1e8e65d0fb5a9a72cd11f2f3c5a9
hanslaptop <hramoscampos@gmail.com>
2023-12-11 00:00:14 -0500

    justify-content agregado

--- js/script.js
+++ js/script.js
@@ -46,10 +46,10 @@
 })(document);
 
 ((d) => {
-    const $botonesRadio = d.querySelectorAll("input[name='flex-wrap-column']");
-    const $cflexWrapColumn = d.getElementById('flex-wrap-column');
+    const $botonesRadio = d.querySelectorAll("input[name='flex-w-column']");
+    const $cflexWrapColumn = d.getElementById('flex-w-column');
     const seleccion = () => {
-        let botonSeleccionado = d.querySelector('input[name="flex-wrap-column"]:checked');
+        let botonSeleccionado = d.querySelector('input[name="flex-w-column"]:checked');
         if (botonSeleccionado.value === "nowrap") {
             $cflexWrapColumn.classList.add("nowrap");
             $cflexWrapColumn.classList.remove("wrap", "wrap-reverse");
@@ -59,6 +59,68 @@
         } else if (botonSeleccionado.value === "wrap-reverse") {
             $cflexWrapColumn.classList.add("wrap-reverse");
             $cflexWrapColumn.classList.remove("nowrap", "wrap");
+        }
+    }
+    $botonesRadio.forEach(boton => {
+        boton.addEventListener("change", seleccion)
+    })
+    seleccion();
+})(document);
+
+((d) => {
+    const $botonesRadio = d.querySelectorAll("input[name='justify-content']");
+    const $cJustifyContent = d.getElementById('justify-content');
+    const seleccion = () => {
+        let botonSeleccionado = d.querySelector('input[name="justify-content"]:checked');
+        if (botonSeleccionado.value === "flex-start") {
+            $cJustifyContent.classList.add("flex-start");
+            $cJustifyContent.classList.remove("flex-end", "center", "space-between", "space-around", "space-evenly");
+        } else if (botonSeleccionado.value === "flex-end") {
+            $cJustifyContent.classList.add("flex-end");
+            $cJustifyContent.classList.remove("flex-start", "center", "space-between", "space-around", "space-evenly");
+        } else if (botonSeleccionado.value === "center") {
+            $cJustifyContent.classList.add("center");
+            $cJustifyContent.classList.remove("flex-start", "flex-end", "space-between", "space-around", "space-evenly");
+        } else if (botonSeleccionado.value === "space-between") {
+            $cJustifyContent.classList.add("space-between");
+            $cJustifyContent.classList.remove("flex-start", "center", "flex-end", "space-around", "space-evenly");
+        } else if (botonSeleccionado.value === "space-around") {
+            $cJustifyContent.classList.add("space-around");
+            $cJustifyContent.classList.remove("flex-start", "center", "space-between", "flex-end", "space-evenly");
+        } else if (botonSeleccionado.value === "space-evenly") {
+            $cJustifyContent.classList.add("space-evenly");
+            $cJustifyContent.classList.remove("flex-start", "center", "space-between", "space-around", "flex-end");
+        }
+    }
+    $botonesRadio.forEach(boton => {
+        boton.addEventListener("change", seleccion)
+    })
+    seleccion();
+})(document);
+
+((d) => {
+    const $botonesRadio = d.querySelectorAll("input[name='justify-c-column']");
+    const $cJustifyContentColumn = d.getElementById('justify-c-column');
+    const seleccion = () => {
+        let botonSeleccionado = d.querySelector('input[name="justify-c-column"]:checked');
+        if (botonSeleccionado.value === "flex-start") {
+            $cJustifyContentColumn.classList.add("flex-start");
+            $cJustifyContentColumn.classList.remove("flex-end", "center", "space-between", "space-around", "space-evenly");
+        } else if (botonSeleccionado.value === "flex-end") {
+            $cJustifyContentColumn.classList.add("flex-end");
+            $cJustifyContentColumn.classList.remove("flex-start", "center", "space-between", "space-around", "space-evenly");
+        } else if (botonSeleccionado.value === "center") {
+            $cJustifyContentColumn.classList.add("center");
+            $cJustifyContentColumn.classList.remove("flex-start", "flex-end", "space-between", "space-around", "space-evenly");
+        } else if (botonSeleccionado.value === "space-between") {
+            $cJustifyContentColumn.classList.add("space-between");
+            $cJustifyContentColumn.classList.remove("flex-start", "center", "flex-end", "space-around", "space-evenly");
+        } else if (botonSeleccionado.value === "space-around") {
+            $cJustifyContentColumn.classList.add("space-around");
+            $cJustifyContentColumn.classList.remove("flex-start", "center", "space-between", "flex-end", "space-evenly");
+        } else if (botonSeleccionado.value === "space-evenly") {
+            $cJustifyContentColumn.classList.add("space-evenly");
+            $cJustifyContentColumn.classList.remove("flex-start", "center", "space-between", "space-around", "flex-end");
         }
     }
     $botonesRadio.forEach(boton => {
